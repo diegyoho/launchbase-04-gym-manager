733f097ef208ca99a733207414df6f892ef1c39a
Diego Moreira <chicodiegomoreira@gmail.com>
2020-04-14 23:19:25 -0300

    Create view stylizing

--- gym-manager/routes.js
+++ gym-manager/routes.js
@@ -2,15 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function (req, res) {
-    res.redirect('/instructors')
+    return res.redirect('/instructors')
 })
 
 routes.get('/instructors', function (req, res) {
-    res.render('instructors/index')
+    return res.render('instructors/index')
+})
+
+routes.get('/instructors/create', function (req, res) {
+    return res.render('instructors/create')
 })
 
 routes.get('/members', function (req, res) {
-    res.render('members/index')
+    return res.render('members/index')
 })
 
 module.exports = routes
