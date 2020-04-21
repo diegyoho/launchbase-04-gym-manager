2111f510420ce6111b5fe89d92aa6f0514ea26f0
Diego Moreira <chicodiegomoreira@gmail.com>
2020-04-21 17:43:46 -0300

    Some Adjusts

--- gym-manager/src/app/models/Instructor.js
+++ gym-manager/src/app/models/Instructor.js
@@ -2,7 +2,9 @@ const db = require('../../config/db')
 
 module.exports = {
     all(callback) {
-        db.query(`SELECT * FROM instructors`, function (err, results) {
+        db.query(`
+            SELECT * FROM instructors 
+            ORDER BY name ASC`, function (err, results) {
             if (err) throw `Database error! ${err}`
 
             callback(results.rows)
--- gym-manager/src/app/models/Member.js
+++ gym-manager/src/app/models/Member.js
@@ -2,7 +2,9 @@ const db = require('../../config/db')
 
 module.exports = {
     all(callback) {
-        db.query(`SELECT * FROM members`, function (err, results) {
+        db.query(`
+            SELECT * FROM members
+            ORDER BY name ASC`, function (err, results) {
             if (err) throw `Database error! ${err}`
 
             callback(results.rows)
